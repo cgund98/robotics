bd3b2cb162392bb47a1145fc180ad403e3a9be10
Callum Gundlach <cgund98@tscstudents.net>
2016-12-06 22:36:11 -0500

    More work

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new'
     post 'login' => 'sessions#create'
     get 'logout' => 'sessions#destroy'
+
+    get 'admin' => "admin#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
